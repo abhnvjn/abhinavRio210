44a5bd5c55eadb3b30fb22d374f0736bd5b8391d
Abhinav Jain <66365287+abhnvjn@users.noreply.github.com>
2020-11-15 21:10:28 +0530

    Update App.java

--- App.java
+++ App.java
@@ -6,5 +6,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "This is Testing DevOps pipeline" );
+        System.out.println( "Hi my name is Abhinav" );
     }
 }
